c5a8d4438fa023184f301d541a3d957391c4325f
junjoons <martin.junyoung@outlook.com>
2020-10-26 16:02:09 +0900

    1.1 Securing Codes

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMSHaWU2EzbQ-2LpMARfiG2tPxboXZACE",
-    authDomain: "nwitter-learning.firebaseapp.com",
-    databaseURL: "https://nwitter-learning.firebaseio.com",
-    projectId: "nwitter-learning",
-    storageBucket: "nwitter-learning.appspot.com",
-    messagingSenderId: "588801109564",
-    appId: "1:588801109564:web:357628e835f34a23329d39"
+    apiKey: process.env.REACT_APP_AUTH_DOMAIN,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 export default firebase.initializeApp(firebaseConfig);
